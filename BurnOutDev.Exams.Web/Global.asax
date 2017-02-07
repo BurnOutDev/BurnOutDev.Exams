34f74c910438005d1b531accc2cde4f4cbd15c40
BurnOutDev <BurnOutDev@BURNOUTDEV>
2017-02-07 19:43:56 +0400

    Add project files.

---
+++ BurnOutDev.Exams.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BurnOutDev.Exams.Web.MvcApplication" Language="C#" %>
---
+++ BurnOutDev.Exams.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BurnOutDev.Exams.Web.Startup))]
+namespace BurnOutDev.Exams.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
